10a6abd9618ace0463b45ee6b34807f7683299f1
Christian Delicaat <c.delicaat@hotmail.com>
2022-02-22 17:17:25 +0100

    Fixed some issues

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,10 +15,13 @@ class Kernel extends BaseKernel
     {
         parent::__construct($environment, $debug);
 
-        $filePath = realpath(glob('build/messageBox*.js')[0]);
-        $contents = file_get_contents($filePath);
-        $contents = str_replace('PHP_REPLACE_HOST', $_SERVER['HTTP_HOST'], $contents);
-        file_put_contents($filePath, $contents);
-        copy($filePath, 'build/messageBox.js');
+        $glob = glob('build/messageBox*.js');
+        if ($glob !== false && \array_key_exists(0, $glob)) {
+            $filePath = realpath($glob[0]);
+            $contents = file_get_contents($filePath);
+            $contents = str_replace('PHP_REPLACE_HOST', $_SERVER['HTTP_HOST'], $contents);
+            file_put_contents($filePath, $contents);
+            copy($filePath, 'build/messageBox.js');
+        }
     }
 }

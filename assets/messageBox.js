b018b1379ea314404b72f968577269f1c2a2a0c3
Christian Delicaat <c.delicaat@hotmail.com>
2022-02-22 12:38:59 +0100

    Cross-Origin messages now working

--- assets/messageBox.js
+++ assets/messageBox.js
@@ -1,34 +1,38 @@
-let button = document.createElement('button');
-button.setAttribute('id', 'messageButton');
-button.addEventListener('click', openPopup);
-button.style.height = '3em';
-button.style.width = '3em';
-button.style.backgroundColor = 'rgb(234 88 12)';
-button.style.borderRadius = '100%';
-button.style.display = 'flex';
-button.style.justifyContent = 'center';
-button.style.alignItems = 'center';
-button.style.position = 'absolute';
-button.style.bottom = '2.5em';
-button.style.right = '2.5em';
-button.style.color = 'white';
+window.onload = function () {
+    let button = document.createElement('button');
+    button.setAttribute('id', 'messageButton');
+    button.addEventListener('click', openPopup);
+    button.style.height = '3em';
+    button.style.width = '3em';
+    button.style.backgroundColor = 'rgb(234 88 12)';
+    button.style.borderRadius = '100%';
+    button.style.display = 'flex';
+    button.style.justifyContent = 'center';
+    button.style.alignItems = 'center';
+    button.style.position = 'absolute';
+    button.style.bottom = '2.5em';
+    button.style.right = '2.5em';
+    button.style.color = 'white';
+    button.style.border = 'none';
+    button.style.cursor = 'pointer';
 
-let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-path.setAttribute('stroke-linecap', 'round');
-path.setAttribute('stroke-linejoin', 'round');
-path.setAttribute('stroke-width', '2');
-path.setAttribute('d', 'M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z');
+    let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('stroke-linecap', 'round');
+    path.setAttribute('stroke-linejoin', 'round');
+    path.setAttribute('stroke-width', '2');
+    path.setAttribute('d', 'M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z');
 
-let icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-icon.setAttribute('fill', 'none');
-icon.setAttribute('viewBox', '0 0 24 24');
-icon.setAttribute('stroke', 'currentColor');
-icon.style.height = '1.5em';
-icon.style.width = '1.5em';
+    let icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    icon.setAttribute('fill', 'none');
+    icon.setAttribute('viewBox', '0 0 24 24');
+    icon.setAttribute('stroke', 'currentColor');
+    icon.style.height = '1.5em';
+    icon.style.width = '1.5em';
 
-icon.appendChild(path);
-button.appendChild(icon)
-document.body.appendChild(button);
+    icon.appendChild(path);
+    button.appendChild(icon)
+    document.body.appendChild(button);
+}
 
 function openPopup() {
     if (typeof Swal == "undefined") {
@@ -69,4 +73,3 @@ function post(url, params) {
     const data = JSON.stringify(params);
     xhr.send(data);
 }
-
--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
@@ -16,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
